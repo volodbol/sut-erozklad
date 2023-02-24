68e245ff6c435646d631dfe2b14090a6e5bd5b06
Volodymyr Bolshakov <volodbol@gmail.com>
2023-02-24 11:11:23 +0200

    fix: rename database class

--- app/src/main/java/com/sut/rozklad/di/DatabaseModule.kt
+++ app/src/main/java/com/sut/rozklad/di/DatabaseModule.kt
@@ -2,7 +2,7 @@ package com.sut.rozklad.di
 
 import android.app.Application
 import androidx.room.Room
-import com.sut.rozklad.feature_academic_group.data.data_source.ClassesDatabase
+import com.sut.rozklad.feature_academic_group.data.data_source.ScheduleDatabase
 import com.sut.rozklad.feature_academic_group.data.repository.LessonRepositoryImpl
 import com.sut.rozklad.feature_academic_group.domain.repository.LessonRepository
 import dagger.Module
@@ -17,17 +17,17 @@ class DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideLessonDatabase(app: Application): ClassesDatabase {
+    fun provideLessonDatabase(app: Application): ScheduleDatabase {
         return Room.databaseBuilder(
             app,
-            ClassesDatabase::class.java,
-            ClassesDatabase.DATABASE_NAME
+            ScheduleDatabase::class.java,
+            ScheduleDatabase.DATABASE_NAME
         ).build()
     }
 
     @Provides
     @Singleton
-    fun provideLessonRepository(db: ClassesDatabase): LessonRepository {
+    fun provideLessonRepository(db: ScheduleDatabase): LessonRepository {
         return LessonRepositoryImpl(db.lessonDao)
     }
 
